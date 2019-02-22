78ba1f49a99add33a01aa126ebc1d647e203dc89
Ryker-Smith <fachtnaroe@gmail.com>
2019-02-22 11:24:32 +0000

    Stuff

--- app/src/main/java/net/fachtnaroe/workviewer/MainActivity.java
+++ app/src/main/java/net/fachtnaroe/workviewer/MainActivity.java
@@ -8,32 +8,52 @@ import com.google.appinventor.components.runtime.Component;
 
 import com.google.appinventor.components.runtime.Button;
 import com.google.appinventor.components.runtime.HorizontalArrangement;
+import com.google.appinventor.components.runtime.Label;
 import com.google.appinventor.components.runtime.VerticalArrangement;
+import com.google.appinventor.components.runtime.WebViewer;
 
 public class MainActivity extends Form implements HandlesEventDispatching
 {
 
-    private Button buttonRefresh;
-    private VerticalArrangement vargParent;
-    private HorizontalArrangement hargTop;
+    Button buttonRefresh;
+    VerticalArrangement vargParent;
+    HorizontalArrangement hargTop;
+    WebViewer webviewMainDisplay;
+    Label padOne, padTwo;
+    String home_url="http://fachtnaroe.net:4567";
 
     protected void $define()
     {
         vargParent =new VerticalArrangement(this);
-        vargParent.WidthPercent(50);
+        vargParent.WidthPercent(100);
         vargParent.HeightPercent(100);
         vargParent.BackgroundColor(COLOR_BLACK );
         hargTop = new HorizontalArrangement(vargParent);
+        hargTop.AlignHorizontal(Component.ALIGNMENT_CENTER);
         hargTop.WidthPercent(100);
         hargTop.Height(50);
+        padOne = new Label(hargTop);
         buttonRefresh = new Button(hargTop);
-        buttonRefresh.Text( "Refresh" );
+        buttonRefresh.Image("buttonRefresh.png");
+        buttonRefresh.Width(50);
         buttonRefresh.Height(50);
+        padTwo=new Label(hargTop);
+        padOne.WidthPercent(40);
+        padTwo.WidthPercent(40);
+        webviewMainDisplay = new WebViewer(vargParent);
+        webviewMainDisplay.ClearCaches();
+        webviewMainDisplay.FollowLinks(true);
+        webviewMainDisplay.HomeUrl(home_url);
+        webviewMainDisplay.HeightPercent(100);
+        webviewMainDisplay.WidthPercent(100);
+        webviewMainDisplay.IgnoreSslErrors(true);
+        webviewMainDisplay.GoHome();
 
         EventDispatcher.registerEventForDelegation( this, formName, "Click" );
         EventDispatcher.registerEventForDelegation( this, formName, "GotText" );
         EventDispatcher.registerEventForDelegation( this, formName, "WebStringChange" );
     }
+
     public boolean dispatchEvent(Component component, String componentName, String eventName, Object[] params )
     {
         if( eventName.equals("Click") ) {
@@ -42,13 +62,11 @@ public class MainActivity extends Form implements HandlesEventDispatching
                 return true;
             }
         }
-        // here is where you'd check for other events of your app...
         return false;
     }
-    // this is the event handler that dispatchEvent above calls. We just set the button background
-    public void RefreshButton_Click()
-    {
 
+    public void RefreshButton_Click() {
+        webviewMainDisplay.GoHome();
     }
 
 }
